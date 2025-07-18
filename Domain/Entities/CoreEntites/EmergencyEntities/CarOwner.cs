58cb031a677027ebefaf20e423980bf540960152
abdo-coco <https://github.com/abdo-coco>
2025-07-19 00:34:36 +0300

    Comments

--- Domain/Entities/CoreEntites/EmergencyEntities/CarOwner.cs
+++ Domain/Entities/CoreEntites/EmergencyEntities/CarOwner.cs
@@ -9,7 +9,7 @@ namespace Domain.Entities.CoreEntites.EmergencyEntities
 {
   public  class CarOwner : IcanOrder
     {
-        public ApplicationUser ApplicationUser { get; set; }
+        public ApplicationUser ApplicationUser { get; set; } 
         public ICollection<ChatSession> ChatSessions { get; set; } = new List<ChatSession>();
         public ICollection<Review> Reviews { get; set; } = new List<Review>();
         public ICollection<EmergencyRequest> EmergencyRequests { get; set; } = new List<EmergencyRequest>();
