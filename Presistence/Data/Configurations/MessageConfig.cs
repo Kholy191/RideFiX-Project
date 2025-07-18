fc5406f41c2a73d462128d6a1c6496c5b66a2264
abdo-coco <https://github.com/abdo-coco>
2025-07-19 00:25:02 +0300

    DbContextConfigration

---
+++ Presistence/Data/Configurations/MessageConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.CoreEntites.EmergencyEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Presistence.Data.Configurations
+{
+    public class MessageConfig : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.HasKey(m => m.Id);
+
+            
+            builder.Property(m => m.Text)
+                   .IsRequired()
+                   .HasMaxLength(1000); 
+
+            builder.Property(m => m.SentAt)
+                   .IsRequired();
+
+            builder.Property(m => m.IsSeen)
+                   .IsRequired();
+
+           
+            builder.HasOne(m => m.ApplicationUser)
+                   .WithMany(m=>m.messages)
+                   .HasForeignKey(m => m.ApplicationId)
+                   .IsRequired()
+                   .OnDelete(DeleteBehavior.NoAction); 
+
+          
+        }
+
+    }
+
+}
