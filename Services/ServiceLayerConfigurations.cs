942f2ac077f64362be479724e167cd5031d1336b
Ahmed ElKholy <a.elkholy2711@gmail.com>
2025-07-18 18:55:33 +0300

    AutoMapper Error Solved

--- Services/ServiceLayerConfigurations.cs
+++ Services/ServiceLayerConfigurations.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ServiceAbstraction;
+using Services.AutoMapperProfile;
+
 
 namespace Services
 {
@@ -8,6 +10,7 @@ namespace Services
         public static IServiceCollection AddServiceConfig(this IServiceCollection Services)
         {
             Services.AddScoped<IServiceManager, ServiceManager>();
+            Services.AddAutoMapper(typeof(ExampleProfile));
             return Services;
         }
     }
