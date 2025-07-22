1dcb93864e7895f4a172020c8018ef4782ccc67f
Ahmed Mamdouh <amamdouh823@gmail.com>
2025-07-23 01:53:03 +0300

    developed updateRequest endpoint to accept or reject the request

---
+++ Services/Specification_Implementation/TechnicianWithAppUserSpec .cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities.CoreEntites.EmergencyEntities;
+using Services.Specification_Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Specification_Implementation
+{
+    public class TechnicianWithAppUserSpec : Specification<Technician, int>
+    {
+        public TechnicianWithAppUserSpec(int technicianId, int pin) : base(t => t.Id == technicianId && t.ApplicationUser.PIN == pin)
+        {
+            AddInclude(t => t.ApplicationUser);
+        }
+    }
+}
