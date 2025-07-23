71ccc3a50bfbc5fa92c955ee093e085adf54c232
Ahmed Mamdouh <amamdouh823@gmail.com>
2025-07-23 10:58:04 +0300

    save changes for TRequest controller

--- RideFix/Controllers/TRequestEmergencyController.cs
+++ RideFix/Controllers/TRequestEmergencyController.cs
@@ -25,10 +25,7 @@ namespace RideFix.Controllers
             var request = await serviceManager.technicianRequestEmergency.GetRequestDetailsByIdAsync(id);
 
             if (request == null)
-            {
-                var errorResponse = ApiResponse<string>.FailResponse("Request not found with this ID");
-                return new ObjectResult(errorResponse) { StatusCode = 404 };
-            }
+                return NotFound(ApiResponse<string>.FailResponse("requestDetails not found with this id"));
 
             return Ok(ApiResponse<EmergencyRequestDetailsDTO>.SuccessResponse(request, "request details found"));
         }
@@ -53,10 +50,7 @@ namespace RideFix.Controllers
 
             var request = await serviceManager.technicianRequestEmergency.GetAllRequestsAssignedToTechnicianAsync(technicianId);
             if (request == null || !request.Any())
-            {
-                var errorResponse = ApiResponse<string>.FailResponse("No requests found for this technician");
-                return new ObjectResult(errorResponse) { StatusCode = 404 };
-            }
+                return NotFound(ApiResponse<string>.FailResponse("technician doesn't have requests"));
             return Ok(ApiResponse<List<EmergencyRequestDetailsDTO>>.SuccessResponse(request, "technician have requests"));
         }
         [HttpPut]
