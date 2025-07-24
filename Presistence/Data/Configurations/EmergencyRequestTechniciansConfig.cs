8bc9d9a873796b7fa8e2ae30cb506b2103b6832b
basma zain <basmazain102000@gmail.com>
2025-07-24 23:40:55 +0300

    final update on database

---
+++ Presistence/Data/Configurations/EmergencyRequestTechniciansConfig.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities.CoreEntites.EmergencyEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presistence.Data.Configurations
+{
+    public class EmergencyRequestTechniciansConfig : IEntityTypeConfiguration<EmergencyRequestTechnicians>
+    {
+        public void Configure(EntityTypeBuilder<EmergencyRequestTechnicians> builder)
+        {
+            builder.HasKey(ert => new { ert.EmergencyRequestId, ert.TechnicianId });
+          
+        }
+    }
+}
---
+++ Services/Exception_Implementation/NotFoundExceptions/RequestNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Exception_Implementation.NotFoundExceptions
+{
+    public class RequestNotFoundException : Exception
+    {
+        public RequestNotFoundException() : base("لا يوجد اي طلبات حاليه للالغاء!")
+        {
+        }
+
+    }
+}
