96faaec992e4ccdad42ce940badd78180ba820a2
Ahmed ElKholy <a.elkholy2711@gmail.com>
2025-07-25 12:12:56 +0300

    Error Solved

--- Shared/DTOs/TechnicianEmergencyRequestDTOs/EmergencyRequestDetailsDTO.cs
+++ Shared/DTOs/TechnicianEmergencyRequestDTOs/EmergencyRequestDetailsDTO.cs
@@ -11,6 +11,7 @@ namespace SharedData.DTOs.TechnicianEmergencyRequestDTOs
     public class EmergencyRequestDetailsDTO
     {
         public bool IsCompleted { get; set; }
+        public int TechnicianId { get; set; }
         public int RequestId { get; set; }
         public string Description { get; set; }
         public string CarOwnerName {  get; set; }
